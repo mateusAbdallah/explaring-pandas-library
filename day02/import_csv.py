f5254ae28fec878c888c05242727f697a955b492
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-15 00:10:27 -0700

    reading csv file and applying filters on dataset

---
+++ day02/import_csv.py
@@ -0,0 +1,48 @@
+# %% 
+
+import pandas as pd
+
+# %%
+#reading csv file
+
+df_customers = pd.read_csv('../data/customers.csv', sep=';')
+df_customers
+
+# %%
+
+df_customers.shape
+
+# %%
+
+df_customers.info(memory_usage='deep')
+
+# %%
+df_customers.info()
+
+# %%
+
+df_customers['Points'].astype(int)
+ # %%
+
+df_customers.describe()
+
+# %%
+# filtering based on certain conditions
+
+condition = df_customers['Points'] > 1500
+condition
+
+df_customers[condition]
+
+# %%
+
+condition = df_customers['Points'] == df_customers['Points'].max()
+df_max_point = df_customers[condition]
+df_max_point
+
+# %%
+# rearrange columns
+
+columns = ['Name', 'UUID', 'Points']
+df_customers[columns]
+
