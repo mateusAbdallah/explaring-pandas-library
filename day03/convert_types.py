ae0424f9109d77dc029a8006ed7e0c8a3f67663b
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-19 22:54:25 -0700

    sorting columns based on points and name

--- day03/convert_types.py
+++ day03/convert_types.py
@@ -7,7 +7,16 @@ import pandas as pd
 df = pd.read_csv('../data/customers.csv', sep=';')
 df
 
-# %%
 
-df = df.sort_values(by='Points', ascending=False)
+# %%
+# sorting based on list
+
+# df./sort_values(by=['Points', 'Name'], ascending=[False, True])
+
+# %%
+#building a simple pipeline to sort and rename columns
+
+df = (df.sort_values(by=['Points', 'Name'], ascending=[False, True])
+    .rename(columns={'UUID':'ID'}))     
 df
+# %%
