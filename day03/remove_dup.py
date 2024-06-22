fe1b6e23ea2bfe2d01509c735716598196e2b0ec
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-21 21:50:00 -0700

    retrieve last transaction from each customers

---
+++ day03/remove_dup.py
@@ -0,0 +1,17 @@
+# %%
+
+import pandas as pd
+
+# %%
+ 
+df = pd.read_excel('../data/transactions.xlsx')
+df
+
+
+# %%
+# getting the last transaction of each customer
+
+df = (df.sort_values(by='DtTransaction', ascending=False)
+    .drop_duplicates(subset=['IdCustomer'])) 
+df
+
