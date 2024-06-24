209a29892fb6b7959c4d3484d890c645549e53c6
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-23 23:14:00 -0700

    get the first name after split based on underscore

--- day03/new_columns.py
+++ day03/new_columns.py
@@ -9,3 +9,14 @@ df = pd.read_csv('../data/customers.csv', sep=';')
 
 df['Points_double'] = df['Points'] * 2
 df
+
+# %%
+
+#get the first name after split based on underscore
+def get_first(nome:str):
+    return nome.split("_")[0]
+
+# %%
+
+df['first_name'] = df['Name'].apply(get_first)
+df
