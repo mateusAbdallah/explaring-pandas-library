87fe9e7c6ea7a27cabfa1bd66b70c46aad428e48
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-26 00:06:35 -0700

    working with missing values

---
+++ day04/na.py
@@ -0,0 +1,24 @@
+# %%
+
+import pandas as pd
+import numpy as np
+
+# %%
+
+data = {
+    "name": ["Leo", "Carol", "Deb", "Poli", "Rafa"],
+    "age": [32, 44, 13, np.nan, 20],
+    "weight": [55.3, 51, 60, np.nan, 90]
+}
+
+df = pd.DataFrame(data)
+df
+
+# %%
+
+df['age'].isna().sum()
+
+# %%
+
+df.isna().sum()
+
