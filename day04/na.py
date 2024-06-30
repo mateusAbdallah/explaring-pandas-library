1a0b54c973a28945e39f233ceb834ac86c4dc4a7
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-29 23:46:58 -0700

    create another dataframe and concat with the previous one

--- day04/na.py
+++ day04/na.py
@@ -39,14 +39,22 @@ df
 
 # %%
 # creating height column
+
 df['height'] = [1.6, 1.54, 1.55, 1.61, 1.7]
 
 # %%
+
+df = df.rename(columns={'weight': 'weight(kg)',
+                        'height': 'height(m)'})
+
+
+# %%
 # define a method to analyze bmi 
+
 def bmi (row):
     
     
-    result = (row['weight'] / row['height']**2)
+    result = (row['weight(kg)'] / row['height(m)']**2)
     
     if result < 18.5:
         status = 'Underweight'
@@ -59,10 +67,6 @@ def bmi (row):
 
     return status
 
-
-
-    
-
 # %%
 
 df['bmi'] = df.apply(bmi, axis=1)
@@ -70,6 +74,22 @@ df
 
 # %%
 
-df = df.rename(columns={'weight': 'weight(kg)',
-                        'height': 'height(m)'})
-df
+new_rows = {
+    'name': ['Mateus', 'Raquel', 'Helena'],
+    'age': [33, 43, 3],
+    'weight(kg)': [73, 70, 15],
+    'height(m)': [1.75, 1.6, 1]
+}
+
+# %%
+
+df_new = pd.DataFrame(new_rows)
+
+# %%
+
+df_new['bmi'] = df_new.apply(bmi, axis=1)
+
+# %%
+
+df_concat = pd.concat([df, df_new]).reset_index(drop=True)
+df_concat
