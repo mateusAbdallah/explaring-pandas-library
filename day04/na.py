d15403543d66821d5960733d3b1e01e0e93285a3
Mateus Abdallah <abdallahmateus@gmail.com>
2024-06-29 22:43:08 -0700

    define method to get bmi

--- day04/na.py
+++ day04/na.py
@@ -35,5 +35,41 @@ df = df.fillna({
     'weight': df["weight"].mean()
 })
 
+df
+
+# %%
+# creating height column
+df['height'] = [1.6, 1.54, 1.55, 1.61, 1.7]
+
+# %%
+# define a method to analyze bmi 
+def bmi (row):
+    
+    
+    result = (row['weight'] / row['height']**2)
+    
+    if result < 18.5:
+        status = 'Underweight'
+    elif 18.5 <= result < 24.9:
+        status = 'Healthy Weight'
+    elif 25.0 <= result < 29.9:
+        status = 'Overweight'
+    else:
+        status = 'Obesity'
+
+    return status
 
 
+
+    
+
+# %%
+
+df['bmi'] = df.apply(bmi, axis=1)
+df
+
+# %%
+
+df = df.rename(columns={'weight': 'weight(kg)',
+                        'height': 'height(m)'})
+df
