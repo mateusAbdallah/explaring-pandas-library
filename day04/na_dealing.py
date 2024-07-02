4649506b50f4eef7fa957ba8a0090398c0f390fc
Mateus Abdallah <abdallahmateus@gmail.com>
2024-07-02 00:13:03 -0700

    using thresh parameter

--- day04/na_dealing.py
+++ day04/na_dealing.py
@@ -8,7 +8,7 @@ import numpy as np
 data = {
     'name':['Joao', 'Jose', 'Maria', 'Margarida', 'Raimundo'],
     'age': [np.nan, 19, np.nan, 42, 33],
-    'weight': [np.nan, 77, 60, 50, np.nan]
+    'weight': [np.nan, 77, 60, np.nan, np.nan]
 }
 
 df = pd.DataFrame(data)
